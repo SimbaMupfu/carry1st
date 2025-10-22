63fd24b99240f7e3c1593d9ff1568b3592ed9747
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-22 23:46:05 +0200

    Add Retrofit instance

--- app/src/main/java/inc/sims/hustles/carry1st/product/di/productsModule.kt
+++ app/src/main/java/inc/sims/hustles/carry1st/product/di/productsModule.kt
@@ -1,7 +1,17 @@
 package inc.sims.hustles.carry1st.product.di
 
+import inc.sims.hustles.carry1st.constants.Constants
+import inc.sims.hustles.carry1st.product.api.ProductApi
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val productsModule = module {
-
+    single {
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .build()
+            .create(ProductApi::class.java)
+    }
 }
