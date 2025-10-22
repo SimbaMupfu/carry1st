33e6faca1a3e671ed8c0db7bf4717959c0ecfa5c
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-23 00:17:44 +0200

    Add functionality to fetch products from api in the repository.

--- app/src/main/java/inc/sims/hustles/carry1st/product/repository/impl/ProductRepositoryImpl.kt
+++ app/src/main/java/inc/sims/hustles/carry1st/product/repository/impl/ProductRepositoryImpl.kt
@@ -5,9 +5,23 @@ import inc.sims.hustles.carry1st.product.model.Product
 import inc.sims.hustles.carry1st.product.repository.ProductRepository
 import inc.sims.hustles.carry1st.product.utils.NetworkState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 
 class ProductRepositoryImpl(private val productApi: ProductApi): ProductRepository {
-    override suspend fun getProductData(): Flow<NetworkState<List<Product>>> {
-        TODO("Not yet implemented")
+    override suspend fun getProductData(): Flow<NetworkState<List<Product>>> = flow{
+        emit(NetworkState.Loading)
+        try {
+            val response = productApi.fetchProducts()
+            if(response.isSuccessful && response.body() != null){
+                emit(NetworkState.Success(response.body()!!))
+            }else {
+                emit(NetworkState.Error("Error: ${response.message()}"))
+            }
+        } catch (ex: HttpException){
+            emit(NetworkState.Error("Error: ${ex.message}", ex.code()))
+        } catch (ex: Exception){
+            emit(NetworkState.Error("Error: ${ex.message}"))
+        }
     }
 }
