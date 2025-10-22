c6164e2d64ca7b67655d70d8a43c1aef88835490
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-22 23:01:35 +0200

    Create an implementation of the Product Repository.

---
+++ app/src/main/java/inc/sims/hustles/carry1st/product/repository/impl/ProductRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package inc.sims.hustles.carry1st.product.repository.impl
+
+import inc.sims.hustles.carry1st.product.api.ProductApi
+import inc.sims.hustles.carry1st.product.model.Product
+import inc.sims.hustles.carry1st.product.repository.ProductRepository
+import inc.sims.hustles.carry1st.product.utils.NetworkState
+import kotlinx.coroutines.flow.Flow
+
+class ProductRepositoryImpl(productApi: ProductApi): ProductRepository {
+    override suspend fun getProductData(): Flow<NetworkState<List<Product>>> {
+        TODO("Not yet implemented")
+    }
+}
