5bc5547ca31887bc6284ebbe092b57fe0a47864e
SimbaMupfu <scmupfururirwa@gmail.com>
2025-10-23 14:37:48 +0200

    Remove unused import from ProductListUI screen.

--- app/src/main/java/inc/sims/hustles/carry1st/product/presentation/ProductListUI.kt
+++ app/src/main/java/inc/sims/hustles/carry1st/product/presentation/ProductListUI.kt
@@ -1,7 +1,6 @@
 package inc.sims.hustles.carry1st.product.presentation
 
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,12 +18,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import inc.sims.hustles.carry1st.R
-//import coil3.compose.AsyncImage
 import inc.sims.hustles.carry1st.product.model.Product
 import inc.sims.hustles.carry1st.product.utils.NetworkState
 import org.koin.androidx.compose.koinViewModel
